442f878764aad62ac0e69081eeb13cbce225c216
Hoosieman <141951470+Hoosieman@users.noreply.github.com>
2024-04-29 16:21:38 -0600

    Update MobileDragAndDropTest.java

--- src/test/java/com/zebrunner/carina/demo/MobileDragAndDropTest.java
+++ src/test/java/com/zebrunner/carina/demo/MobileDragAndDropTest.java
@@ -3,22 +3,37 @@ package com.zebrunner.carina.demo;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.zebrunner.carina.core.IAbstractTest;
-import com.zebrunner.carina.demo.mobile.gui.pages.android.DragAndDropPage;
 import com.zebrunner.agent.core.annotation.TestLabel;
+import com.zebrunner.carina.core.IAbstractTest;
 import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import com.zebrunner.carina.demo.mobile.gui.pages.android.DragAndDropPage;
 import com.zebrunner.carina.utils.R;
 
 public class MobileDragAndDropTest implements IAbstractTest {
+
+
+
+    
+
+
+
+
+
 
     @Test()
     @MethodOwner(owner = "qpsdemo")
     @TestLabel(name = "feature", value = {"mobile", "acceptance"})
     public void testDragAndDrop() {
-        R.CONFIG.put("capabilities.app",
-                "https://github.com/appium/java-client/raw/master/src/test/resources/apps/ApiDemos-debug.apk",
-                true);
-        R.CONFIG.put("capabilities.appActivity", ".view.DragAndDropDemo", true);
+        R.CONFIG.put("capabilities.platformName", "Android");
+        R.CONFIG.put("capabilities.deviceName", "emulator-5554"); // Replace "emulator-5554" with the name of your emulated device
+        R.CONFIG.put("capabilities.appium:address", "10.35.11.208"); // Specify the IP address of the machine running the Appium server
+        R.CONFIG.put("capabilities.appium:port", "4723"); // Specify the port of the Appium server
+        R.CONFIG.put("capabilities.app", "https://github.com/appium/java-client/raw/master/src/test/resources/apps/ApiDemos-debug.apk");
+        R.CONFIG.put("capabilities.appActivity", ".view.DragAndDropDemo");
+        R.CONFIG.put("capabilities.automationName", "UiAutomator2");
+
+
+        
 
         DragAndDropPage dragAndDropPage = new DragAndDropPage(getDriver());
         dragAndDropPage.dragDown();
@@ -26,5 +41,8 @@ public class MobileDragAndDropTest implements IAbstractTest {
         dragAndDropPage.dragDiagonal();
         Assert.assertTrue(dragAndDropPage.isDragAndDropMessagePresent(), "Should be provided pop up message after successful drag and drop");
     }
+
+
+
 
 }
